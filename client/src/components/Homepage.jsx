1e5c9d22fadb6ddfbeb2ddcbf35ca9c29c14df19
Vishal <vishal.v2024cse@sece.ac.in>
2026-04-06 10:42:37 +0530

    Initial Setup

---
+++ client/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  return (
+    <div className="flex items-start justify-center min-h-screen pt-10">
+      <h1 className="text-6xl font-extrabold text-blue-600">
+        Task Flow
+      </h1>
+    </div>
+  );
+};
+
+export default Homepage;
